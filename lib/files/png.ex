31ab1832d1099de527650e1a1c80689fd9890f2b
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-07-14 20:01:59 -0400

    module constants

--- lib/files/png.ex
+++ lib/files/png.ex
@@ -2,12 +2,12 @@ defmodule Absinthe.Files.PNG do
   alias __MODULE__
 
   @moduledoc """
-  89 50 4E 47 0D 0A 1A 0A are the first 8 bytes in the PNG signature (PNG\\r\\n0x1A\\n).
+  <<0x89 0x50 0x4E 0x47 0x0D 0x0A 0x1A 0x0A>> are the first 8 bytes in the PNG signature
   """
 
   defstruct [:width, :height, :bit_depth, :color_type, :compression, :filter, :interlace, :chunks]
 
-  def parse_png(
+  def decode(
         <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, length::size(32), "IHDR",
           width::size(32), height::size(32), bit_depth, color_type, compression_method,
           filter_method, interlace_method, crc::size(32), chunks::binary>>
@@ -23,10 +23,10 @@ defmodule Absinthe.Files.PNG do
       chunks: []
     }
 
-    parse_png_chunks(chunks, png)
+    decode_chunks(chunks, png)
   end
 
-  defp parse_png_chunks(
+  defp decode_chunks(
          <<length::size(32), chunk_type::size(32), chunk_data::binary-size(length), crc::size(32),
            chunks::binary>>,
          png
@@ -37,7 +37,9 @@ defmodule Absinthe.Files.PNG do
     parse_png_chunks(chunks, png)
   end
 
-  defp parse_png_chunks(<<>>, png) do
+  defp decode_chunks(<<>>, png) do
     %{png | chunks: Enum.reverse(png.chunks)}
   end
+
+  def png_header(), do: <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>
 end
--- lib/guards/guards.ex
+++ lib/guards/guards.ex
@@ -1,10 +1,10 @@
 defmodule Absinthe.Guards do
-  defguard is_jpg(value) when binary_part(value, 0, 2) == <<0xFF, 0xD8>>
+  defguard is_JPG(value) when binary_part(value, 0, 2) == <<255, 216>>
 
-  defguard is_jfif(value)
+  defguard is_JFIF(value)
            when binary_part(value, 0, 4) ==
-                  <<0xFF, 0xD8, 0xFF, 0xE0>>
+                  <<255, 216, 255, 224>>
 
-  defguard is_png(value)
-           when binary_part(value, 0, 8) == <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A>>
+  defguard is_PNG(value)
+           when binary_part(value, 0, 8) == <<137, 80, 78, 71, 13, 10, 26, 10>>
 end
